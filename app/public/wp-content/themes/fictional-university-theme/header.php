2389a821ac24b1be39243b5331ec045bdc901479
iren <irasibil2@gmail.com>
2022-07-27 18:15:47 +0300

    made layout adaptive

--- app/public/wp-content/themes/fictional-university-theme/header.php
+++ app/public/wp-content/themes/fictional-university-theme/header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
-    <!-- <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
 </head>
-<body>
+<!-- body_class function add a number of classes which can help to style the page -->
+<body <?php body_class(); ?>>
     <header class="site-header">
         <div class="container">
             <h1 class="school-logo-text float-left">
