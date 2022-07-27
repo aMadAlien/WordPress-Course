4df1d62bc1c55a509a8b762f577e91a88290ce46
iren <irasibil2@gmail.com>
2022-07-27 16:25:28 +0300

    work with parent and child pages
    created child dependencies from parent page

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -9,7 +9,7 @@
         <!-- the pic into 1st (Privacy Policy) section) -->
         <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>)"></div>
         <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"><?php echo "Privacy Policy" ?></h1>
+            <h1 class="page-banner__title"><?php echo the_title(); ?></h1>
             <div class="page-banner__intro">
                 <p>DON'T FORGET TO REPLACE ME LATER</p>
             </div>
@@ -17,11 +17,19 @@
     </div>
 
     <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p>
-                <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
-            </p>
-        </div>
+        <?php
+            $theParent = wp_get_post_parent_id(get_the_ID());
+            if($theParent) { ?>
+
+                <div class="metabox metabox--position-up metabox--with-home-link">
+                    <p>
+                        <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a>
+                        <span class="metabox__main"><?php echo the_title(); ?></span>
+                    </p>
+                </div>
+
+            <?php }
+        ?>
 
         <!-- 
         <div class="page-links">
