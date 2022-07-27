57af66b269d8f90718686fa99cb64b85f6a52860
iren <irasibil2@gmail.com>
2022-07-27 17:25:04 +0300

    work with sidebar menu
    made dependencies in sidebar menu for children and parent pages;
    change the order of about children pages

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -1,10 +1,8 @@
-<?php
-
-    get_header();
-
+<?php get_header();
     while(have_posts()) {
         the_post(); ?>
 
+    <!-- main screen -->
     <div class="page-banner">
         <!-- the pic into 1st (Privacy Policy) section) -->
         <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>)"></div>
@@ -16,38 +14,59 @@
         </div>
     </div>
 
+    <!-- 2 btns under main screen -->
     <div class="container container--narrow page-section">
         <?php
             $theParent = wp_get_post_parent_id(get_the_ID());
             if($theParent) { ?>
-
                 <div class="metabox metabox--position-up metabox--with-home-link">
                     <p>
                         <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a>
                         <span class="metabox__main"><?php echo the_title(); ?></span>
                     </p>
                 </div>
-
             <?php }
         ?>
 
-        <!-- 
+
+        <!-- MENU SIDEBAR -->
+        <?php 
+        // get list of pages but only in memory
+        $testArray = get_pages(array(
+            'child_of' => get_the_ID() 
+        ));
+
+        if($theParent or $testArray) { ?>
         <div class="page-links">
-            <h2 class="page-links__title"><a href="#">About Us</a></h2>
+            <!-- the menu title and its link depends on the parent -->
+            <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
             <ul class="min-list">
-                <li class="current_page_item"><a href="#">Our History</a></li>
-                <li><a href="#">Our Goals</a></li>
+                <?php
+
+                // children page-links will appear depending on parent
+                    if($theParent) {
+                        $findChildrenOf = $theParent;
+                    } else {
+                        $findChildrenOf = get_the_ID();
+                    }
+
+                    // get and print list of the pages
+                    wp_list_pages(array(
+                        // remote work 'Pages'
+                        'title_li' => NULL,
+                        // add all children-page-links
+                        'child_of' => $findChildrenOf,
+                        // change the order of pages. It has to be written correct order in wordpress editor too
+                        'sort_column' => 'menu_order'
+                    ));
+                ?>
             </ul>
         </div>
-        -->
+        <?php } ?>
 
         <div class="generic-content">
             <?php the_content(); ?>
         </div>
     </div>
 
-    <?php }
-
-    get_footer();
-
-?>
+<?php } get_footer(); ?>
