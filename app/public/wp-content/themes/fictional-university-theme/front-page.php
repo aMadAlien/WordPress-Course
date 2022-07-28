15f9764ecf7383a473316e2cd00303b983bc11d8
iren <irasibil2@gmail.com>
2022-07-28 10:31:21 +0300

    fixed btn and link

--- app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -70,7 +70,7 @@
                     <!-- after looping through a separate query, this function restores the $post global to the current post in the main query. -->
                     <?php } wp_reset_postdata();
                 ?>
-                <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+                <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
             </div>
         </div>
     </div>
