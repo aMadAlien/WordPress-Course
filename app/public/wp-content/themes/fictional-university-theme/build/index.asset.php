899cae58848c8e6caa269fe4299dd09e950dd341
iren <irasibil2@gmail.com>
2022-08-04 09:28:02 +0300

    enfonced limit of one like per user

--- app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
+++ app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'b4fcaa8286fc03f0b198');
+<?php return array('dependencies' => array('jquery'), 'version' => 'a78e29a5d612a0a7f3d0');
--- app/public/wp-content/themes/fictional-university-theme/single-professor.php
+++ app/public/wp-content/themes/fictional-university-theme/single-professor.php
@@ -32,22 +32,25 @@
                                 // did someone like this post? - status "no"
                                 $existStatus = 'no';
 
-                                $existQuery = new WP_Query(array(
-                                    'author' => get_current_user_id(),
-                                    'post_type' => 'like',
-                                    'meta_query' => array(
-                                        array(
-                                            'key' => 'liked_professor_id',
-                                            'compare' => '=',
-                                            'value' => get_the_ID()
+                                if(is_user_logged_in()) {
+                                    $existQuery = new WP_Query(array(
+                                        'author' => get_current_user_id(),
+                                        'post_type' => 'like',
+                                        'meta_query' => array(
+                                            array(
+                                                'key' => 'liked_professor_id',
+                                                'compare' => '=',
+                                                'value' => get_the_ID()
+                                            )
                                         )
-                                    )
-                                ));
+                                    ));
 
-                                // if someone liked this post - status "yes"
-                                if($existQuery -> found_posts) {
-                                    $existStatus = 'yes';
+                                    // if someone liked this post - status "yes"
+                                    if($existQuery -> found_posts) {
+                                        $existStatus = 'yes';
+                                    }
                                 }
+
 
                             ?>
                             <!-- data-exists="" => check if a professor has user like already or not -->
