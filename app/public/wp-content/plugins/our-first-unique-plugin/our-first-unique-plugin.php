c2305d5f3b49a398433228373c80139ace009528
iren <irasibil2@gmail.com>
2022-08-06 20:30:50 +0300

    created settings forms

--- app/public/wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ app/public/wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -16,14 +16,51 @@ class WordCountAndTimePlugin {
 
     function settings() {
         add_settings_section("wcp_first_section", null, null, 'word-count-settings-page');
+        // adds "display location" parameter
         add_settings_field('wcp_location', 'Display location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section');
-        register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => 'sanitize_text_field', 'default' => '0'));
+        register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => array($this, 'sanitizeLocation'), 'default' => '0'));
+
+        // adds "headline text" parameter
+        add_settings_field('wcp_headline', 'Headline Text', array($this, 'headlineHTML'), 'word-count-settings-page', 'wcp_first_section');
+        register_setting('wordcountplugin', 'wcp_headline', array('sanitize_callback' => 'sanitize_text_field', 'default' => 'Post Statictics'));
+
+        // adds "word count" parameter
+        add_settings_field('wcp_wordcount', 'Word Count', array($this, 'checkboxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName' => 'wcp_wordcount'));
+        register_setting('wordcountplugin', 'wcp_wordcount', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
+
+        // adds "character count" parameter
+        add_settings_field('wcp_charactercount', 'Character Count', array($this, 'checkboxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName' => 'wcp_charactercount'));
+        register_setting('wordcountplugin', 'wcp_charactercount', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
+
+        // adds "read time" parameter
+        add_settings_field('wcp_readtime', 'Read Time', array($this, 'checkboxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName' => 'wcp_readtime'));
+        register_setting('wordcountplugin', 'wcp_readtime', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
     }
 
+    function sanitizeLocation($input) {
+        if ($input != '0' AND $input != '1') {
+            add_settings_error('wcp_location', 'wcp_location_error', 'Display location must be either beginning or end.');
+            return get_option('wcp_location');
+        }
+        return $input;
+    }
+
+    // adds "word count" parameter
+    function checkboxHTML($args) { ?>
+        <input type="checkbox" name="<?php echo $args['theName'] ?>" value="1" <?php checked(get_option($args['theName']), '1'); ?>>
+    <?php }
+
+    // adds "headline text" parameter
+    function headlineHTML() { ?>
+        <input type="text" name="wcp_headline" value="<?php echo esc_attr(get_option('wcp_headline')); ?>">
+    <?php }
+
+    // adds "display location" parameter
     function locationHTML() { ?>
     <select name="wcp_location">
-        <option value="0">Beginning of post</option>
-        <option value="1">End of post</option>
+        <!-- selected(...) save chosen in admin-plugin-pannel parameter -->
+        <option value="0" <?php selected(get_option('wcp_location'), '0'); ?>>Beginning of post</option>
+        <option value="1" <?php selected(get_option('wcp_location'), '1'); ?>>End of post</option>
     </select>
     <?php }
 
