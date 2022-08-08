87764e07a557a23059a3ebcb2b02cb04f5b60e04
iren <irasibil2@gmail.com>
2022-08-08 15:43:55 +0300

    created func 'select a professor'
    work with data base

--- app/public/wp-content/plugins/featured-professor/build/index.asset.php
+++ app/public/wp-content/plugins/featured-professor/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => 'b697d39b75505c37484c');
+<?php return array('dependencies' => array('wp-data', 'wp-element'), 'version' => 'd1f48ec3bc1dfbf999d9');
