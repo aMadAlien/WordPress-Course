a05631bbb9cfab3f184f408c72b0766ac0ab5512
iren <irasibil2@gmail.com>
2022-08-08 12:00:28 +0300

    created function 'disable incorrect answers'
    disable incorrect answers when correct one in selected;
    correct mistake with "incorrect" answers.

--- app/public/wp-content/plugins/are-you-paying-attention/build/frontend.asset.php
+++ app/public/wp-content/plugins/are-you-paying-attention/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '4866c283b0f21729bd11');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'df56cffa2894da8a312f');
--- app/public/wp-content/plugins/are-you-paying-attention/src/frontend.js
+++ app/public/wp-content/plugins/are-you-paying-attention/src/frontend.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import ReactDOM from 'react-dom'
 import "./frontend.scss"
 
@@ -15,6 +15,22 @@ divsToUpdate.forEach(function(div) {
 // contains the content of the block (quiz)
 function Quiz(props) {
     const [isCorrect, setIsCorrect] = useState(undefined)
+    const [isCorrectDelayed, setIsCorrectDelayed] = useState(undefined)
+
+    // if a selected answer is incorrect, wait 2.6s and refresh
+    useEffect(() => {
+        if (isCorrect === false) {
+            setTimeout(() => {
+                setIsCorrect(undefined)
+            }, 2600)
+        }
+
+        if (isCorrect == true) {
+            setTimeout(() => {
+                setIsCorrectDelayed(true)
+            }, 1000)
+        }
+    }, [isCorrect])
 
     function handleAnswer(index) {
         if (index == props.correctAnswer) {
@@ -30,11 +46,33 @@ function Quiz(props) {
             <p>{props.question}</p>
             <ul>
                 {props.answers.map(function(answer, index) {
-                    return <li onClick={() => handleAnswer(index)}>{answer}</li>
+                    return (
+                    // if selected incorrect answer it retrieves "no-click" class (-> happends nothing)
+                    <li className={(isCorrectDelayed === true && index == props.correctAnswer ? "no-click" : "") + 
+                    // if selected correct answer - incorrect others retrieve "fade-incorrect" class
+                    (isCorrectDelayed === true && index != props.correctAnswer ? "fade-incorrect" : "")} 
+                    onClick={isCorrect ===true ? undefined : () => handleAnswer(index) }>
+                        {/* if correct answer renders: */}
+                        {isCorrectDelayed === true && index == props.correctAnswer && (
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="bi bi-check" viewBox="0 0 16 16">
+                                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+                            </svg>
+                        )}
+                        {/* if imcorrect answer renders: */}
+                        {isCorrectDelayed === true && index != props.correctAnswer && (
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="bi bi-x" viewBox="0 0 16 16">
+                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                            </svg>
+                        )}
+                        {/* ANSWER CONTENT */}
+                        {answer}
+                    </li>
+                    )
                 })}
             </ul>
 
             {/* MESSAGE "CORRECT ANSWER" */}
+            {/* disable incorrect answers when correct one in selected */}
             <div className={"correct-message" + (isCorrect == true ? " correct-message--visible" : "")}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="bi bi-emoji-smile" viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
