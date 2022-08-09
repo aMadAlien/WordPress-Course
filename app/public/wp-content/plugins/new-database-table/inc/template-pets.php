e8012b423a101836bc08c8f366d26f9ea7aaebf0
iren <irasibil2@gmail.com>
2022-08-09 09:34:58 +0300

    prepared the table

--- app/public/wp-content/plugins/new-database-table/inc/template-pets.php
+++ app/public/wp-content/plugins/new-database-table/inc/template-pets.php
@@ -2,6 +2,7 @@
 
 get_header(); ?>
 
+<!-- MAIN SCREEN -->
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
   <div class="page-banner__content container container--narrow">
@@ -12,10 +13,19 @@ get_header(); ?>
   </div>  
 </div>
 
+<!-- CONTENT -->
 <div class="container container--narrow page-section">
 
   <p>This page took <strong><?php echo timer_stop();?></strong> seconds to prepare. Found <strong>x</strong> results (showing the first x).</p>
 
+  <?php
+    global $wpdb;
+    $tablename = $wpdb->prefix . 'pets';
+    $ourQuery = $wpdb->prepare("SELECT * from $tablename LIMIT 10");
+    $pets = $wpdb->get_results($ourQuery);
+  ?>
+
+  <!-- DB TABLE -->
   <table class="pet-adoption-table">
     <tr>
       <th>Name</th>
@@ -26,15 +36,19 @@ get_header(); ?>
       <th>Favorite Color</th>
       <th>Favorite Food</th>
     </tr>
-    <tr>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-    </tr>
+    <?php
+      foreach($pets as $pet) { ?>
+        <tr>
+          <td><?php echo $pet->petname ?></td>
+          <td><?php echo $pet->species ?></td>
+          <td><?php echo $pet->petweight ?></td>
+          <td><?php echo $pet->birthyear ?></td>
+          <td><?php echo $pet->favhobby ?></td>
+          <td><?php echo $pet->favcolor ?></td>
+          <td><?php echo $pet->favfood ?></td>
+        </tr>
+      <?php }
+    ?>
   </table>
   
 </div>
