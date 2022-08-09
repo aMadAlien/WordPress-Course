6342beb96f77f9b9fdeacaadeb07875013f884ca
iren <irasibil2@gmail.com>
2022-08-09 17:56:45 +0300

    work with theme.json
    added capability to change screen width;
    added theme colors;
    remove typography and button settings.

--- app/public/wp-content/themes/fictional-block-theme/build/banner.asset.php
+++ app/public/wp-content/themes/fictional-block-theme/build/banner.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-element'), 'version' => 'e51093f4d46dd2832cc4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-element'), 'version' => '02fd8ebbffafa89aca61');
--- app/public/wp-content/themes/fictional-block-theme/our-blocks/banner.js
+++ app/public/wp-content/themes/fictional-block-theme/our-blocks/banner.js
@@ -2,6 +2,12 @@ import { InnerBlocks } from "@wordpress/block-editor"
 
 wp.blocks.registerBlockType("ourblocktheme/banner", {
     title: "Banner",
+    supports: {
+        align: ["full"]
+    },
+    attributes: {
+        align: {type: "string", default: "full"}
+    },
     edit: EditComponent,
     save: SaveComponent
 })
